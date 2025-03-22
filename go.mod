68b531f259882569bacc383b972b34e02bdeac2e
rahulkookal <rahulkkodoth@gmail.com>
2025-03-22 12:06:40 +0000

    build

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module rahul.com/secure-exam
 go 1.22.0
 
 require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/gin-contrib/cors v1.7.3
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-errors/errors v1.5.1
 	github.com/golang-jwt/jwt/v4 v4.5.1
 	github.com/sirupsen/logrus v1.9.3
-	github.com/spf13/cobra v1.9.1
 	go.mongodb.org/mongo-driver v1.17.2
 	golang.org/x/crypto v0.33.0
 )
@@ -24,9 +25,8 @@ require (
 	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -35,7 +35,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,81 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "rahul.com/secure-exam/cmd"
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"rahul.com/secure-exam/internal/routes"
+)
+
+var ginLambda *ginadapter.GinLambda
 
 func main() {
-	cmd.Execute()
+	server := gin.Default()
+	server.Use(LogrusLogger(), gin.Recovery())
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Change this for production
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Register routes
+	routes.RegisterExamRoutes(server)
+	routes.RegisterUserRoutes(server)
+	routes.RegisterEventRoutes(server)
+	routes.RegisterOrganisationRoutes(server)
+
+	// Convert Gin to AWS Lambda Proxy
+	ginLambda = ginadapter.New(server)
+
+	// If running locally, start normally
+	if isLocal() {
+		server.Run(":8080")
+	} else {
+		// Run as AWS Lambda function
+		lambda.Start(Handler)
+	}
+}
+
+// AWS Lambda Handler
+func Handler(req interface{}) (interface{}, error) {
+	// Type assertion to APIGatewayProxyRequest
+	if request, ok := req.(events.APIGatewayProxyRequest); ok {
+		return ginLambda.Proxy(request)
+	}
+	return nil, fmt.Errorf("invalid request type")
+}
+
+// Logging Middleware
+func LogrusLogger() gin.HandlerFunc {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+
+		logger.WithFields(logrus.Fields{
+			"time":       time.Now().Format(time.RFC3339),
+			"status":     c.Writer.Status(),
+			"latency":    time.Since(start).String(),
+			"client_ip":  c.ClientIP(),
+			"method":     c.Request.Method,
+			"path":       c.Request.URL.Path,
+			"user_agent": c.Request.UserAgent(),
+		}).Info("Request Processed")
+	}
+}
+
+// Helper to check if running locally
+func isLocal() bool {
+	// Set to `false` in production
+	return true
 }
